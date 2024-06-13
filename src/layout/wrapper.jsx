37004da0c1fe8bfb3fe51bc8128af9a5a5ab5033
pkkwilliam <24216676+pkkwilliam@users.noreply.github.com>
2024-06-13 10:30:58 -0700

    add google tag

--- src/layout/wrapper.jsx
+++ src/layout/wrapper.jsx
@@ -1,19 +1,38 @@
-import React, { useEffect } from 'react';
-import { animationCreate } from '../utils/utils';
-import BackToTopCom from '@components/common/scroll-to-top';
+import React, { useEffect } from "react";
+import { animationCreate } from "../utils/utils";
+import BackToTopCom from "@components/common/scroll-to-top";
 
-const Wrapper = ({children}) => {
+const Wrapper = ({ children }) => {
   useEffect(() => {
     setTimeout(() => {
-      animationCreate()
+      animationCreate();
     }, 200);
-  },[])
+    loadGoogleAdTag();
+  }, []);
   return (
     <>
       {children}
-      <BackToTopCom/>
+      <BackToTopCom />
     </>
   );
+};
+
+const loadGoogleAdTag = () => {
+  // Create the script tag for gtag.js
+  const scriptTag = document.createElement("script");
+  scriptTag.src = "https://www.googletagmanager.com/gtag/js?id=AW-16588567823";
+  scriptTag.async = true;
+  document.head.appendChild(scriptTag);
+
+  // Create the inline script for initializing gtag
+  const inlineScript = document.createElement("script");
+  inlineScript.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'AW-16588567823');
+  `;
+  document.head.appendChild(inlineScript);
 };
 
 export default Wrapper;
